8ba7f840291843dbe5756bcbb2a5e88117d3d827
maximums0427 <wangxiaoqin0427@gmail.com>
2016-02-18 00:29:45 +0800

    array and string

---
+++ Array and String/UniqueChar.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 00:24:28 2016
+
+@author: xiaoqin
+"""
+
+#Implement an algorithm to determine if a string has all unique
+# characters. What if you can not use additional data structure
